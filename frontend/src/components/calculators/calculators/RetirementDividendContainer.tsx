0d45bc57f2ebd9c8116f0cc18faf4cfa44a1d0e3
samuel-kar <samuel.karlhager@hotmail.com>
2025-10-17 13:02:17 +0200

    change rows in dividend retirement to 2*2 instead of 4*1 for cards to avoid squeeze on desktop

--- frontend/src/components/calculators/calculators/RetirementDividendContainer.tsx
+++ frontend/src/components/calculators/calculators/RetirementDividendContainer.tsx
@@ -93,7 +93,7 @@ export default function RetirementDividendContainer() {
       {/* Results Section */}
       <div className="md:col-span-3 space-y-6">
         {/* Top Cards */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           <div
             className="p-4 rounded-lg border"
             style={{
