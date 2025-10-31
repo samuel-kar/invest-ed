e3929e5a380c62dbaedb75865ac4cadd5d17c64e
samuel-kar <samuel.karlhager@hotmail.com>
2025-10-31 10:27:34 +0100

    add firefox and apple/webkit to playwright testing

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -15,5 +15,13 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
   ],
 })
